eb79a82ed4155d5143d4bbb4a092348d7296abba
HarshPratapSingh1 <harshpratap715@gmail.com>
2025-03-21 19:35:27 +0530

    21/3/25

---
+++ src/codeForces/BuildingAnAquarium.java
@@ -0,0 +1,42 @@
+package codeForces;
+import java.util.*;
+public class BuildingAnAquarium {
+	public static void main(String[]args) {
+		Scanner scan=new Scanner(System.in);
+		int t=scan.nextInt();
+		while(t-->0) {
+			int n=scan.nextInt();
+			long x=scan.nextLong();
+			
+			long arr[]=new long [n];
+			for(int i=0;i<n;i++)arr[i]=scan.nextLong();
+			
+			Arrays.sort(arr);
+			
+			long l=1;
+			long r=(int)1e10;
+			
+			long mid;
+//			long ans=0;
+			while(l<=r) {
+				mid=l+(r-l)/2;
+				if(isPosi(mid,arr,x)) {
+//					ans=mid;
+					r=mid-1;
+				}
+				else {
+					l=mid+1;
+				}
+			}
+			System.out.println(r);
+		}
+	}
+	public static boolean isPosi(long mid,long arr[],long x) {
+		long sum=0;
+		for(int i=0;i<arr.length;i++) {
+			if(mid>arr[i])sum+=(mid-arr[i]);
+			
+		}
+		 return sum>x;
+	}
+}
--- src/codeForces/CollectingGame.java
+++ src/codeForces/CollectingGame.java
@@ -48,6 +48,7 @@ public class CollectingGame {
 	        }
 	        System.out.println();
 	    }
+		
 	}
 	}
 
