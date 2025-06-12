7bc3a5d1a0d50029382e0f7a286b7545ffe92ee0
HarshPratapSingh1 <harshpratap715@gmail.com>
2025-06-13 02:03:05 +0530

    12 june 3rd

--- src/codeForces/AverageSleepTime.java
+++ src/codeForces/AverageSleepTime.java
@@ -12,20 +12,19 @@ public class AverageSleepTime {
 		
 		for(int i = 0 ; i < n ; i++) arr[i] = scan.nextInt();
 		
-		int prefix[] = new int[n];
+		long prefix[] = new long[n];
 		prefix[0] = arr[0];
 		
 		for(int i = 1 ; i < n ; i++) prefix[i] = arr[i] + prefix[i - 1];
 		
 		int i = k - 1;
-		System.out.println(Arrays.toString(prefix));
 		long ans = 0;
+		
 		while(i < n) {
 			ans += (prefix[i] -  ((i - k >= 0) ? prefix[i - k] : 0)); 
 			i++;
-//			System.out.println(ans);
 		}
-//		System.out.println(ans);
-		System.out.println((double)ans/(n - k + 1));
+
+		System.out.println((double)ans/((long)(n - k + 1)));
 	}
 }
