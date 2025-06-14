89f53973afad4cbe2aec41390042a1eff567bc2d
HarshPratapSingh1 <harshpratap715@gmail.com>
2025-06-14 14:23:18 +0530

    14 june 1st

---
+++ src/codeForces/AlyonaAndANarrowFridge.java
@@ -0,0 +1,35 @@
+package codeForces;
+import java.util.*;
+public class AlyonaAndANarrowFridge {
+	public static void main(String[]args) {
+		Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0 ; i < n ; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        int answer = 0;
+
+        for (int i = 0 ; i <= n ; i++) {
+            int[] temp = Arrays.copyOfRange(arr, 0, i);
+            Arrays.sort(temp);
+            int sum = 0;
+
+            for (int j = temp.length - 1 ; j >= 0 ; j -= 2) {
+                sum += temp[j];
+            }
+
+            if (sum <= k) {
+                answer = i;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
