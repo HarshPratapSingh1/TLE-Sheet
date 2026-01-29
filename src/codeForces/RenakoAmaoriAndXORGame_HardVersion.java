7f22b310c5f1f32967d65ddd5be42833290dd954
HarshPratapSingh1 <harshpratap715@gmail.com>
2026-01-29 19:22:22 +0530

    practice

---
+++ src/codeForces/RenakoAmaoriAndXORGame_HardVersion.java
@@ -0,0 +1,139 @@
+import java.util.*;
+import java.io.*;
+
+public class RenakoAmaoriAndXORGame_HardVersion {
+    static FastIO scan;
+    static final int MOD = 1_000_000_007;
+    static final long LINF = (long) 1e18;
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input.txt"));
+        System.setOut(new PrintStream("output.txt"));
+        scan = new FastIO();
+        int t = 1;
+        t = scan.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+        scan.close();
+    }
+
+    static void solve() throws IOException {
+        int n = scan.nextInt();
+        long arr[] = new long[n];
+        long brr[] = new long[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = scan.nextLong();
+        for (int i = 0; i < n; i++)
+            brr[i] = scan.nextLong();
+
+        long xor = 0;
+
+        for (int i = 0; i < n; i++) {
+            xor ^= arr[i];
+            xor ^= brr[i];
+        }
+        // for(int i = 0; i < n; i++) System.out.print((arr[i]^brr[i])+" ");
+        // System.out.println();
+        if (xor == 0) {
+            System.out.println("Tie");
+        } else {
+            long msb = Long.highestOneBit(xor);
+
+            for (int i = n - 1; i >= 0; i--) {
+                if (((arr[i] ^ brr[i]) & msb) != 0) {
+                    System.out.println((i % 2 == 0) ? "Ajisai" : "Mai");
+                    break;
+                }
+            }
+        }
+
+    }
+
+    // ---------------------- FAST I/O ----------------------
+    static class FastIO {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+        private final PrintWriter out = new PrintWriter(System.out);
+
+        private int readByte() throws IOException {
+            if (ptr >= len) {
+                ptr = 0;
+                len = in.read(buffer);
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        public String next() throws IOException {
+            int c = readByte();
+            while (c >= 0 && c <= 32)
+                c = readByte();
+            if (c == -1)
+                return null;
+            StringBuilder sb = new StringBuilder();
+            while (c > 32) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int c = readByte();
+            while (c >= 0 && c <= 32)
+                c = readByte();
+            if (c == -1)
+                return 0;
+            int s = 1;
+            if (c == '-') {
+                s = -1;
+                c = readByte();
+            }
+            int v = 0;
+            while (c > 32) {
+                v = v * 10 + (c - '0');
+                c = readByte();
+            }
+            return v * s;
+        }
+
+        public long nextLong() throws IOException {
+            int c = readByte();
+            while (c >= 0 && c <= 32)
+                c = readByte();
+            if (c == -1)
+                return 0;
+            int s = 1;
+            if (c == '-') {
+                s = -1;
+                c = readByte();
+            }
+            long v = 0;
+            while (c > 32) {
+                v = v * 10 + (c - '0');
+                c = readByte();
+            }
+            return v * s;
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public void pn(Object o) {
+            out.println(o);
+        }
+
+        public void p(Object o) {
+            out.print(o);
+        }
+
+        public void close() {
+            out.close();
+        }
+    }
+}
